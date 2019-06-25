21b6c74e09c9011cf9a423fda1fdfab77d20150f
ELDOR ERGASHEV <eldor9012545@gmail.com>
2019-06-25 03:10:27 -0900

    changes in route

--- routes/web.php
+++ routes/web.php
@@ -38,16 +38,18 @@ Route::get('/search', 'SearchController@search')->name('search');
 //show result
 Route::get('/search/{search_panel}/show', 'SearchController@showProduct')->name('search.show');
 
-// products refactoring
-Route::get('home/{id}/edit', 'HomeController@edit')->name('showProduct');
-
 // market update
-Route::PUT('home/{id}/update', 'HomeController@update')->name('home/update');
-
 Route::group(['middleware' => 'auth'], function()
 {
+    // products refactoring
+    Route::get('home/{id}/edit', 'HomeController@edit')->name('showProduct');
+
+    Route::PUT('home/{id}/update', 'HomeController@update')->name('home/update');
+
     Route::get('/admin', 'adminController@admin')->middleware('admin');
     // Refactoring All Products
+
+
     Route::get('/controlProducts', 'adminController@controlProducts')->name('controlProducts');
     Route::DELETE('/controlProducts/', 'adminController@destroyProduct')->name('admin.destroyProduct');
 
@@ -57,6 +59,7 @@ Route::group(['middleware' => 'auth'], function()
     Route::PUT('/controlProducts', 'adminController@UpdateApprovement')->name('controlProducts.UpdateApprovement');
     Route::get('/getAjaxContent', 'adminController@getAjaxContent')->name('getAjaxContent');
 });
+
 
 Route::get('/getContentSportMaster', 'ParserController@getContentSportMaster')->name('getContentSportMaster');
 Route::get('/getGloariyaJeans', 'ParserController@getGloariyaJeans')->name('getGloariyaJeans');
